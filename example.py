58d6163809156dcf78187fead1a920b7e49ae8dd
Ketan Babar <ketan.babar04@gmail.com>
2025-12-01 20:10:42 +0530

    Added new Python File

---
+++ example.py
@@ -0,0 +1,17 @@
+#Numeric data types in Python
+int_num = 10
+float_num = 10.5
+complex_num = 2 + 3j
+print("Integer:", int_num)
+print("Float:", float_num)
+print("Complex Number:", complex_num)
+
+#Arithmetic operations
+sum_result = int_num + float_num
+diff_result = float_num - int_num
+prod_result = int_num * 2
+div_result = float_num / 2
+print("Sum:", sum_result)
+print("Difference:", diff_result)
+print("Product:", prod_result)
+print("Division:", div_result)
