4c775988d15d1bd024943f742c0eab251a8a3533
Ketan Babar <ketan.babar04@gmail.com>
2025-12-03 12:36:20 +0530

    If else learning

--- If else statement.py
+++ If else statement.py
@@ -1,3 +1,5 @@
+#example 1
+
 x = 7
 if x > 10:
     print("x is greater than 10")
@@ -5,3 +7,14 @@ elif x >5:
     print("x is greater than 5 but less than or equal to 10")
 else:
     print("x is not greater than 5")
+
+#example 2
+
+x = 15
+if x > 10:
+    print("x is greater than 10")
+else:
+    if x > 5:
+        print("x is greater than 5 but less than or equal to 10")
+    else:
+        print("x is not greater than 5")
